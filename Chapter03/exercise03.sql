1ba73d910bbdce69f3cfecf95e760050c24d2292
BhabhaSeptember <septemberbhabha@gmail.com>
2025-02-25 07:59:32 +0200

    commit

---
+++ Chapter03/exercise03.sql
@@ -0,0 +1,25 @@
+-- 1) 
+-- I would use numeric(4, 1) because no driver travels more than 999 miles 
+-- and we need an accuracy of one digit after the decimal point
+-- therefore precision must be 4 and scale of 1
+-- We use fixed decimals instead of floating for better accuracy
+
+-- 2) 
+-- last_name varchar(30)
+-- first_name varchar(25)
+-- We will use varchar so as to accommodate for varying lengths of names
+-- We must separate the first and last names for more flexible data querying/analysis/handling down the line
+
+-- 3)
+-- We should get an error since the format is not correct for a timestamp data type
+
+
+
+
+
+
+
+
+
+
+
